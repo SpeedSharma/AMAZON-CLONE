1541bf34eea240b95e2d9e4ba481fc6f56967b84
Speed Sharma <speedsniper919@gmail.com>
2020-09-23 12:43:23 +0530

    Updates

---
+++ precache-manifest.d03003061779736ae063fbd13da27c86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "349373d5d8e091523ee37efdde9b4c24",
+    "url": "/AMAZON-CLONE/index.html"
+  },
+  {
+    "revision": "2194a9c6a25f5e06e5fa",
+    "url": "/AMAZON-CLONE/static/css/main.f1460991.chunk.css"
+  },
+  {
+    "revision": "c7c88fd036a982ca6079",
+    "url": "/AMAZON-CLONE/static/js/2.85bf13c4.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/AMAZON-CLONE/static/js/2.85bf13c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2194a9c6a25f5e06e5fa",
+    "url": "/AMAZON-CLONE/static/js/main.8b9e7ec1.chunk.js"
+  },
+  {
+    "revision": "83c00b7d544cc1b4aa69",
+    "url": "/AMAZON-CLONE/static/js/runtime-main.9a4b199a.js"
+  }
+]);
